b0d39fc8474cc2beb085ba01c61365fd28c35024
Dillon <dilloncortez@gmail.com>
2017-06-23 12:49:55 -0600

    reverted back to old sidekiq config

--- config/initializers/1_sidekiq.rb
+++ config/initializers/1_sidekiq.rb
@@ -1,40 +1,12 @@
 require 'sidekiq/web'
 
-# config = {
-#   host: Redis.current.client.host,
-#   port: Redis.current.client.port,
-#   password: Redis.current.client.password,
-#   db: (!!defined?(REDIS_CONFIG) ? REDIS_CONFIG[:db_worker] : Redis.current.client.db),
-#   namespace: "sidekiq"
-# }
-#
-# Sidekiq.configure_server { |c| c.redis = config }
-# Sidekiq.configure_client { |c| c.redis = config }
+config = {
+  host: Redis.current.client.host,
+  port: Redis.current.client.port,
+  password: Redis.current.client.password,
+  db: (!!defined?(REDIS_CONFIG) ? REDIS_CONFIG[:db_worker] : Redis.current.client.db),
+  namespace: "sidekiq"
+}
 
-
-if Rails.env.production?
-
-  Sidekiq.configure_client do |config|
-    config.redis = { url: ENV['REDIS_URL']}
-  end
-
-  Sidekiq.configure_server do |config|
-    config.redis = { url: ENV['REDIS_URL']}
-
-    Rails.application.config.after_initialize do
-      Rails.logger.info("DB Connection Pool size for Sidekiq Server before disconnect is: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
-      ActiveRecord::Base.connection_pool.disconnect!
-
-      ActiveSupport.on_load(:active_record) do
-        config = Rails.application.config.database_configuration[Rails.env]
-        config['reaping_frequency'] = ENV['DATABASE_REAP_FREQ'] || 10 # seconds
-        # config['pool'] = ENV['WORKER_DB_POOL_SIZE'] || Sidekiq.options[:concurrency]
-        config['pool'] = 16
-        ActiveRecord::Base.establish_connection(config)
-
-        Rails.logger.info("DB Connection Pool size for Sidekiq Server is now: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
-      end
-    end
-  end
-
-end
+Sidekiq.configure_server { |c| c.redis = config }
+Sidekiq.configure_client { |c| c.redis = config }
